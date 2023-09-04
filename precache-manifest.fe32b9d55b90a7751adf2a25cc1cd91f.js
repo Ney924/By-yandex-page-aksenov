ec46e211d758f2c53da300c71f8e21fdc5490145
Ilya Aksenov <ais@yandex.ru>
2023-09-04 23:56:12 +0300

    Updates

---
+++ precache-manifest.fe32b9d55b90a7751adf2a25cc1cd91f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "800442592881cbea3051",
+    "url": "/By-yandex-page-aksenov/static/css/main.521a4cee.chunk.css"
+  },
+  {
+    "revision": "800442592881cbea3051",
+    "url": "/By-yandex-page-aksenov/static/js/main.1dbe73ed.chunk.js"
+  },
+  {
+    "revision": "da818f52fae55f8fe13b",
+    "url": "/By-yandex-page-aksenov/static/js/runtime~main.9dc08968.js"
+  },
+  {
+    "revision": "4281965fbc5416caec9e",
+    "url": "/By-yandex-page-aksenov/static/css/2.b498cc4d.chunk.css"
+  },
+  {
+    "revision": "4281965fbc5416caec9e",
+    "url": "/By-yandex-page-aksenov/static/js/2.bfdbbcda.chunk.js"
+  },
+  {
+    "revision": "ea7ded9f1f5094e14392",
+    "url": "/By-yandex-page-aksenov/static/js/3.d433891d.chunk.js"
+  },
+  {
+    "revision": "56deb8b131f49b04e6d6ba36d24e5a13",
+    "url": "/By-yandex-page-aksenov/static/media/photo1.56deb8b1.jpg"
+  },
+  {
+    "revision": "0e66f5f40fb01c8b1146e3ca9b96f211",
+    "url": "/By-yandex-page-aksenov/static/media/photo2.0e66f5f4.jpg"
+  },
+  {
+    "revision": "04050098db485aa30df089d87bac625a",
+    "url": "/By-yandex-page-aksenov/index.html"
+  }
+];
