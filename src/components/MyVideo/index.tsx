ec686320adbcd6d1d06a5ff54c97f7dd0170de06
Ilya Aksenov <ais@yandex.ru>
2023-09-04 23:15:28 +0300

    add video

--- src/components/MyVideo/index.tsx
+++ src/components/MyVideo/index.tsx
@@ -4,12 +4,12 @@ import './style.css'
 export const MyVideo = () => {
   return (
     <div className="my-video">
-      {/* <video
+      <video
         src='videos/video1.mp4'
         width="600"
         height="400"
         controls
-      /> */}
+      />
       Тут скоро будет видео
     </div>
   );
